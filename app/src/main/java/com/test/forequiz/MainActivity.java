c86cb19199c4568a0294d9f2bd5aa4fc54093644
Julia <berrylily.0709@gmail.com>
2017-05-25 21:40:26 -0700

    Implement choice buttons

--- app/src/main/java/com/test/forequiz/MainActivity.java
+++ app/src/main/java/com/test/forequiz/MainActivity.java
@@ -1,13 +1,32 @@
 package com.test.forequiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    // array for choices
+    String[] QUIZ = {"ant","mouse","gorilla","whale"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // shuffle positions
+        List<String> list = Arrays.asList(QUIZ.clone());
+        Collections.shuffle(list);
+        // get choices from above list
+        ((Button)findViewById(R.id.b0)).setText(list.get(0));
+        ((Button)findViewById(R.id.b1)).setText(list.get(1));
+        ((Button)findViewById(R.id.b2)).setText(list.get(2));
+        ((Button)findViewById(R.id.b3)).setText(list.get(3));
     }
+
 }
+
